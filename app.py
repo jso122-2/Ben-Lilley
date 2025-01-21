0eab2116f53fa4813efbc2d7d15f52b2cf579148
unknown <jacksondev122@gmail.com>
2025-01-22 01:29:07 +1100

    Updated app.py with new deployment settings

--- app.py
+++ app.py
@@ -295,4 +295,5 @@ def customer_profiles():
     return render_template('customer_profiles.html', profiles=profiles)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
